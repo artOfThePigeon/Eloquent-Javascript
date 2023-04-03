e57cae977097f6420875eebbb02229714614e147
David Lev Wilson <42543340+artOfThePigeon@users.noreply.github.com>
2023-04-02 22:00:16 -0500

    counting characters that belong to each script, and identifying the percentage of each script.

--- Part 1 Language/ch5/higherOrderFunctions.js
+++ Part 1 Language/ch5/higherOrderFunctions.js
@@ -227,5 +227,67 @@ for (let char of roseDragon) {
 // can use codePointAt(0) to get its code.
 
 
+// Recognizing Text
+
+// Counting characters that belong to each script
+function countBy(items, groupName) {
+	let counts =[];
+	for (let item of items) {
+		let name = groupName(item);
+		let known = counts.findIndex(c => c.name == name);
+		if (known == -1) {
+			counts.push({name, count: 1});
+		} else {
+			counts[known].count++;
+		}
+	}
+	return counts;
+}
+console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
+// → 0: {name: false, count: 2}
+//   1: {name: true, count: 3}
+
+// Use countBy to tell us which scripts are used in a piece of text
+function textScripts(text) {
+	let scripts = countBy(text, char => {
+		let script = characterScript(char.codePointAt(0));
+		return script ? script.name : "none";
+	}).filter(({name}) => name != "none");
+	let total = scripts.reduce((n, {count}) => n + count, 0);
+	if (total == 0) return "No scripts found";
+
+	return scripts.map(({name, count}) => {
+		return `${Math.round(count * 100 / total)}% ${name}`;
+	}).join(", ");
+}
+
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+// → 61% Han, 22% Latin, 17% Cyrillic
+
+// EXERCISES
+
+// use the reduce method in combination with the concat method to "flatten" array 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
