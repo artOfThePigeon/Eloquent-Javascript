70b763319998448cdfae430e0f8d9462be01b0ac
David Lev Wilson <42543340+artOfThePigeon@users.noreply.github.com>
2023-04-04 21:46:05 -0500

    completed exercises 1 through 3, but have not checked answers

--- Part 1 Language/ch5/higherOrderFunctions.js
+++ Part 1 Language/ch5/higherOrderFunctions.js
@@ -193,6 +193,10 @@ function characterScript(code) {
 	}
 	return null;
 }
+
+console.log(characterScript(121));
+// → {name: "Latin", …}
+
 // the 'some' method is another higher order function. It takes a test function and
 // tells you whether that function returns true for any of the elements in the array
 
@@ -266,11 +270,76 @@ console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
 
 // EXERCISES
 
-// use the reduce method in combination with the concat method to "flatten" array 
+// #1 Flattening ---
+
+/*
+use the reduce method in combination with the concat method to "flatten"  an array 
+of arrays into a single array that has all the elements of the original arrays.
+*/
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays.reduce((a, b) => a.concat(b)));
+// → [1, 2, 3, 4, 5, 6]
 
 
+// #2 Your Own Loop
 
+/*
+Write a higher-order function loop that provides something like a for loop statement. 
+It takes a value, a test function, an update function, and a body function. Each 
+iteration, it first runs the test function on the current loop value and stops if
+that returns false. Then it calls the body function, giving it the current value. 
+Finally, it calls the update function to create a new value and starts from the beginning.
+*/
 
+function loop(count, test, update, body) {
+	while (test(count)) {
+		body(count);
+		count = update(count);
+	}
+}
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
+
+// #3 Everything
+
+/* 
+Analogous to the some method, arrays also have an every method. This one returns true 
+when the given function returns true for every element in the array. In a way, some is a 
+version of the || operator that acts on arrays, and every is like the && operator.
+
+Implement every as a function that takes an array and a predicate function as parameters. 
+Write two versions, one using a loop and one using the some method.
+*/
+
+// Function #1 with loop
+function every(array, test) {
+	for (let i of array) {
+		if (!test(i) == true) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Function #2 with 'some' method
+function every(array, test) {
+	if (array.some(i => {
+		return !test(i)
+	})) {
+		return false;
+	} else {
+		return true;
+	}
+}
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
 
 
 
